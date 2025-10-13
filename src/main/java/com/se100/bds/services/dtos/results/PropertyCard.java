ddb24a294a3fd27fbff0fd25dbe08d11eafb3b7a
Minh dep trai <phandinhminh48@gmail.com>
2025-10-13 23:41:41 +0700

    added mongo2

--- src/main/java/com/se100/bds/services/dtos/results/PropertyCard.java
+++ src/main/java/com/se100/bds/services/dtos/results/PropertyCard.java
@@ -2,6 +2,7 @@ package com.se100.bds.services.dtos.results;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.math.BigDecimal;
@@ -9,6 +10,7 @@ import java.util.UUID;
 
 @Getter
 @Setter
+@NoArgsConstructor
 @AllArgsConstructor
 public class PropertyCard {
     private UUID id;
