6634b29411a2354bccf773cea1a03baa49797740
Minh dep trai <phandinhminh48@gmail.com>
2025-11-16 12:39:45 +0700

    add external payment

---
+++ src/main/java/com/se100/bds/services/payment/gateway/AppotaPayGateway.java
@@ -0,0 +1,11 @@
+package com.se100.bds.services.payment.gateway;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+@RequiredArgsConstructor
+public class AppotaPayGateway {
+}
---
+++ src/main/java/com/se100/bds/services/payment/impl/AppotaPayStrategy.java
@@ -0,0 +1,4 @@
+package com.se100.bds.services.payment.impl;
+
+public class AppotaPayStrategy {
+}
